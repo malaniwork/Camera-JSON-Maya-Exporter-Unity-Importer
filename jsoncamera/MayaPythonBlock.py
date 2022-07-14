d1984ef363e9877b38ab9d869c96682e7d4aeba9
Maysam <alani.maysam@gmail.com>
2022-07-14 17:46:59 -0500

    Revert "Files"
    This reverts commit b692e401c9d18b4a0caf0d8765398d0c753b05e7.

--- jsoncamera/MayaPythonBlock.py
+++
@@ -1,9 +0,0 @@
-###### Please use this script in Maya. This script will run the exportCamera def from the MayaCamera script
-
-import maya.cmds as cmds
-import tdcjson.MayaCamera as mc
-from importlib import reload
-
-
-reload(mc)
-mc.exportCamera(camera=cmds.ls(type='camera',sl=True))
